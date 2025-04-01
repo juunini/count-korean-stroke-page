61ca836f0dab58433c9d327f3cfa8d5ee222b186
juunini <juuni.ni.i@gmail.com>
2025-04-01 21:23:40 +0900

    add assets path in svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			strict: true,
+		}),
+		paths: {
+			assets: 'https://ko-stroke.netlify.app',
+		}, 
+	}
 };
 
 export default config;
